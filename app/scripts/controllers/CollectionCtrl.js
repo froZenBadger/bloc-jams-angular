51a4633507f91709a5730f840a7480530b5d15fd
Chris Palmer <cpalmer608@gmail.com>
2017-04-15 23:38:04 -0500

    Checkpoint 11 ng filters: completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,7 +1,21 @@
 (function() {
      function CollectionCtrl(Fixtures) {
         this.albums = Fixtures.getCollection(12);
-        
+
+        var currentAlbum = Fixtures.getAlbum();
+
+        var albumTime = function(albumObj) {
+            var total = 0;
+            for (let i = 0; i < albumObj.songs.length; i++) {
+                total += albumObj.songs[i].duration;
+            }
+            return total;
+        };
+            
+        this.getTime = function() {
+            return (albumTime(currentAlbum));
+        }; 
+
      }
  
      angular
